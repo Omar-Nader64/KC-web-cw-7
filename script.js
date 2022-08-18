c5d93f7a330fb4c5d8973626e33a1719fcbc1164
Ahmed-Nader64 <100669445+Omar-Nader64@users.noreply.github.com>
2022-08-18 15:41:43 +0300

    done

--- script.js
+++ script.js
@@ -11,12 +11,21 @@ function BMI (weight=80, height=1.83) {
 function Status(bmi) {
 
   if(bmi < 18.5){
-     return "لديك نقص في الوزن";
+    document.getElementById("result").style.color = "orange"
+    document.getElementById("container").style.backgroundColor = "rgb(255, 229, 180)"
+    return "لديك نقص في الوزن";
+     
+     
 
   }else if (bmi >= 18.5 && bmi < 25) {
+    document.getElementById("result").style.color = "green";
+    document.getElementById("container").style.backgroundColor = "rgb(154, 255, 154)"
     return "وزنك صحي";
+   
 
   }else{
+    document.getElementById("result").style.color = "red"
+    document.getElementById("container").style.backgroundColor = "rgb(255, 129, 129)"
     return "لديك زيادة في الوزن";
   }
 }
